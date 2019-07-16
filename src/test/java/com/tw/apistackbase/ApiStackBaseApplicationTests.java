9ebdb70c47f5fcf48eee1c3395a8d29f6174b032
MrLinZHui <742847492@qq.com>
2019-07-16 17:47:22 +0800

    should_return_200_when_get_Employees

--- src/test/java/com/tw/apistackbase/ApiStackBaseApplicationTests.java
+++ src/test/java/com/tw/apistackbase/ApiStackBaseApplicationTests.java
@@ -2,13 +2,33 @@ package com.tw.apistackbase;
 
 import org.junit.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-@ExtendWith(SpringExtension.class)
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class ApiStackBaseApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+	@Test
+	public void should_return_200_when_get_Employees() throws Exception {
+		this.mockMvc.perform(get("/employees")).andDo(print()).andExpect(status().isOk()).
+		andExpect(content().string(containsString("[{\"age\":18,\"id\":0,\"name\":\"zhangsan\",\"gender\":\"male\"}]")));
+	}
 	@Test
 	public void contextLoads() {
 		
